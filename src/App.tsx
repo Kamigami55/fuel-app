5f976f5778499ade3607bba6c6888c2f1bd757f1
Kamigami55 <eason@easonchang.com>
2021-05-30 13:44:59 +0800

    Add dark theme by Material-UI
    https://material-ui.com/customization/palette/#dark-mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import '@fontsource/roboto';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import './App.global.css';
@@ -9,12 +11,27 @@ import './App.global.css';
 import Timer from './components/Timer';
 
 export default function App() {
+  // Dark theme by Material-UI
+  // ref: https://material-ui.com/customization/palette/#dark-mode
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
     <Router>
-      <CssBaseline />
-      <Switch>
-        <Route path="/" component={Timer} />
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Switch>
+          <Route path="/" component={Timer} />
+        </Switch>
+      </ThemeProvider>
     </Router>
   );
 }
